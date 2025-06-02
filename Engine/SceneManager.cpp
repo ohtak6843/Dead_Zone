2fc91f0a05fb7639230efc430363ca0fe64ca1c4
Lucianne0424 <pwj0424@naver.com>
2025-06-02 16:06:26 +0900

    SceneManager에 있는 좀비 출력 부분 주석처리

--- Engine/SceneManager.cpp
+++ Engine/SceneManager.cpp
@@ -668,27 +668,27 @@ shared_ptr<Scene> SceneManager::LoadTestScene()
 //
 //#pragma endregion
 
-#pragma region Zombie
-	{
-		shared_ptr<MeshData> meshData = GET_SINGLE(Resources)->LoadFBX(L"..\\Resources\\FBX\\NormalZombie.fbx");
-
-		for (int i = 0; i < 10; i++)
-		{
-			vector<shared_ptr<GameObject>> gameObjects = meshData->Instantiate();
-
-			for (auto& gameObject : gameObjects)
-			{
-				gameObject->SetName(L"Zombie");
-				gameObject->SetCheckFrustum(false);
-				gameObject->GetTransform()->SetLocalPosition(Vec3(-800.f + (160.f * i), 70.f, 2000.f));
-				gameObject->GetTransform()->SetLocalScale(Vec3(2.f, 2.f, 2.f));
-				gameObject->GetTransform()->SetLocalRotation(Vec3(-90.f, 0.f, 0.f));
-				scene->AddGameObject(gameObject);
-				gameObject->AddComponent(make_shared<Zombie>());
-			}
-		}
-	}
-#pragma endregion
+//#pragma region Zombie
+//	{
+//		shared_ptr<MeshData> meshData = GET_SINGLE(Resources)->LoadFBX(L"..\\Resources\\FBX\\NormalZombie.fbx");
+//
+//		for (int i = 0; i < 10; i++)
+//		{
+//			vector<shared_ptr<GameObject>> gameObjects = meshData->Instantiate();
+//
+//			for (auto& gameObject : gameObjects)
+//			{
+//				gameObject->SetName(L"Zombie");
+//				gameObject->SetCheckFrustum(false);
+//				gameObject->GetTransform()->SetLocalPosition(Vec3(-800.f + (160.f * i), 70.f, 2000.f));
+//				gameObject->GetTransform()->SetLocalScale(Vec3(2.f, 2.f, 2.f));
+//				gameObject->GetTransform()->SetLocalRotation(Vec3(-90.f, 0.f, 0.f));
+//				scene->AddGameObject(gameObject);
+//				gameObject->AddComponent(make_shared<Zombie>());
+//			}
+//		}
+//	}
+//#pragma endregion
 
 #pragma region TestFBX
 	{
